75757ed1348ed034fad6b67490fd48c44310bf5b
AlexHoratio <horatioesf@protonmail.com>
2019-04-09 12:59:06 +0100

    added cancel button

--- Scripts/buttons.gd
+++ Scripts/buttons.gd
@@ -21,11 +21,10 @@ func _on_harvest_pressed():
 		get_node("harvest/harvest").play()
 	else:
 		get_node("harvest/cantharvest").play()
-		
-		
 
 func _process(delta):
 	get_node("harvest").disabled = not(current_plot_harvestable)
+	get_node("cancel").disabled = ((get_node("../inspector").selected_plot_id == "-1") or (data.get_value("Plots", get_node("../inspector").selected_plot_id, null) == null))
 
 func _on_relax_pressed():
 	destination = "res://Scenes/relax.tscn"
@@ -34,3 +33,15 @@ func _on_relax_pressed():
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene(destination)
+
+func _on_cancel_pressed():
+	if(not(get_node("../inspector").selected_plot_id == "-1")):
+		var plot = get_node("../flowerbed/plot" + str(get_node("../inspector").selected_plot_id))
+		data.erase_value("Plots", get_node("../inspector").selected_plot_id)
+		countdown_manager.remove_countdown_by_id(get_node("../inspector").selected_plot_id)
+		get_node("../flowerbed").harvest_plant_at_plot(get_node("../inspector").selected_plot_id)
+		get_node("../inspector").deselect_plot(get_node("../inspector").selected_plot_id)
+		#plot.get_node("sprite").queue_free()
+		get_node("cancel/cancel").play()
+	else:
+		get_node("cancel/cantcancel").play()
